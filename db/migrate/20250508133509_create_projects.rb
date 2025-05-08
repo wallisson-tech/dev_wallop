45fbe12e82ecc3f2440e378c7d8d545ce435e5e7
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-05-08 10:38:52 -0300

    Criar o scaffold de projetos

---
+++ db/migrate/20250508133509_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[7.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+      t.string :link
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
